4970bed686b48b1a3e996015525c8c7931203df3
nabilr <nabil.rahiman@nyu.edu>
2017-03-12 19:12:20 +0400

    Initial commit

--- src/multipoll.py
+++ src/multipoll.py
@@ -7,6 +7,8 @@ monitoring them
 
 
 #from mininet.topo import SingleSwitchTopo
+import subprocess
+import select
 import random
 from mininet.topo import Topo
 from mininet.net import Mininet
@@ -27,11 +29,41 @@ from mininet.log import setLogLevel, info
 
 user_list = []
 
-cmd_list = ['USER', 'PASS', 'PWD', '!PWD','CD','!CD', 'PWD', '!PWD','GET','PUT', 'QUIT']
+cmd_list = ['USER', 'PASS', 'PWD', '!PWD','CD','!CD', 'PWD', '!PWD','GET','GET', 'PUT', 'QUIT']
 
-user_list = [ ('Nabil','1234'), ('Brooke','qwer'), ('Martin','iluvnet'),('Yasir','ethernet')]
-server_files = ['bigfile', 'small_file1']
+#user_list = [ ('Nabil','1234'), ('Brooke','qwer'), ('Martin','iluvnet'),('Yasir','ethernet')]
+user_list = [ ('dave','dave'), ('himal', 'himal'), ('nabil', 'password'),('yasir', 'password')]
+server_files = ['bigfile', 'smallfile']
 dir_list = ['h', 'level1']
+
+
+ftp_prompt = "ftp>"
+
+
+
+def expect(poll, f):
+
+    found_expected = False
+    multi_line = ""
+    while True:
+        r = poll.poll(500)
+        if r:
+            multi_line += f.stdout.read(1)
+            
+        else:
+            print 'Time out ......'
+            break
+
+    multi_line = multi_line.split('\n')
+    for line in multi_line:
+        print 'logging...',list(line)
+        if line.startswith(ftp_prompt, 0, len(ftp_prompt)):
+            print 'found prompt'
+            return True
+
+
+    return False
+    
 
 class SingleSwitchTopo(Topo):
     "Single switch connected to n hosts."
@@ -68,7 +100,7 @@ def monitorTest( N=4, seconds=20 ):
     print "../obj/FTPserver " +  server.IP() + " 5000 &"
 
     
-    ftp_cmd,cmds, outfiles, errfiles, infiles = {}, {}, {}, {}, {}
+    in_sequence, poll, pipe_out, ftp_cmd,cmds, outfiles, errfiles, infiles = {}, {}, {}, {}, {}, {}, {}, {}
     for h in hosts[1:]:
         # Create and/or erase output files
         outfiles[ h ] = '/tmp/%s.out' % h.name
@@ -78,7 +110,14 @@ def monitorTest( N=4, seconds=20 ):
         h.cmd( 'echo >', errfiles[ h ] )
         h.cmd( 'echo >', infiles[ h ] )
         h.cmd('tail -f %s | stdbuf -o 0   ../obj/FTPclient %s %s > %s &'%(infiles[h], server.IP(), '5000', outfiles[ h ] ))
+        pipe_out[h] = subprocess.Popen(['tail','-F',outfiles[ h ]],
+            stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        poll[h] = select.poll()
+        poll[h].register( pipe_out[h].stdout)
+        #poll[h].register( pipe_out[h].stdout, READ_ONLY  )
+        expect( poll[h] ,  pipe_out[h])
         ftp_cmd[h] = [] + cmd_list
+        in_sequence[h] = True
 
 
 
@@ -98,34 +137,45 @@ def monitorTest( N=4, seconds=20 ):
 
         print i, tmp_hosts[i]
         h = tmp_hosts[i]
+        if in_sequence[h] == False:
+            h.cmd('echo  >> %s' %(infiles[h]))
+            in_sequence[h] = expect( poll[h] ,  pipe_out[h])
+            continue
+            
         if len(ftp_cmd[h]) > 0:
             sub_cmd = ftp_cmd[h].pop(0)
             print tmp_hosts[i].cmd("ps -ef | egrep 'FTP|tail'");
             if sub_cmd == 'USER':
                 print h.name, 'USER'
-                h.cmd('echo >> %s' %(infiles[h]))
                 h.cmd('echo USER %s >> %s' %(user_list[i][0], infiles[h]))
+                in_sequence[h] = expect( poll[h] ,  pipe_out[h])
+                
                 sleep(1);
             if sub_cmd == 'PASS':
                 print h.name, 'PASS'
                 h.cmd('echo  PASS %s >> %s' %(user_list[i][1], infiles[h]))
                 sleep(1);
+                in_sequence[h] = expect( poll[h] ,  pipe_out[h])
             if sub_cmd == '!CD':
                 print h.name, '!CD'
                 print h.cmd('echo  "!""CD %s" >> %s' %(h.name, infiles[h]))
                 sleep(1);
+                in_sequence[h] = expect( poll[h] ,  pipe_out[h])
             if sub_cmd == 'CD':
                 print h.name, 'CD'
                 h.cmd('echo  CD %s >> %s' %(server.name, infiles[h]))
                 sleep(1);
+                in_sequence[h] = expect( poll[h] ,  pipe_out[h])
             if sub_cmd == '!PWD':
                 print h.name, '!PWD'
                 h.cmd('echo  "!""PWD >> %s"' %(infiles[h]))
                 sleep(1);
+                in_sequence[h] = expect( poll[h] ,  pipe_out[h])
             if sub_cmd == 'PWD':
                 print h.name, 'PWD'
                 h.cmd('echo  PWD >> %s' %(infiles[h]))
                 sleep(1);
+                in_sequence[h] = expect( poll[h] ,  pipe_out[h])
             if sub_cmd == 'GET':
                 print h.name, 'GET'
                 if len(server_files) == 2:
@@ -135,14 +185,7 @@ def monitorTest( N=4, seconds=20 ):
                     smallfile = server_files[0];
                     h.cmd('echo  GET %s >> %s' %(smallfile, infiles[h]))
                 sleep(1);
-            if sub_cmd == 'PUT':
-                print h.name, 'PUT'
-            if sub_cmd == 'QUIT':
-                print h.name, 'QUIT'
-                h.cmd('echo QUIT >> %s' %(infiles[h]))
-                sleep(1);
-            if sub_cmd == 'GET':
-                print h.name, 'GET'
+                in_sequence[h] = expect( poll[h] ,  pipe_out[h])
             if sub_cmd == 'PUT':
                 print h.name, 'PUT'
             if sub_cmd == 'QUIT':
@@ -151,13 +194,6 @@ def monitorTest( N=4, seconds=20 ):
                 sleep(1);
         else:
             tmp_hosts.pop(i)
-    output = server.cmd(' ls h2/in h3/in h4/in')
-
-    print output
-
-    while 'No such file' in output:
-        print 'Sleep......'
-        sleep(1);
     for h in hosts:
         h.cmd('pkill FTPserver')
         h.cmd('pkill FTPclient')
